468088970fc929755e748e074a5415532dd33578
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-07-12 19:29:39 +0500

    For Loop

---
+++ loops.js
@@ -0,0 +1,48 @@
+// loops are used to execute the piece of code multiple times
+
+
+
+for(let i=0; i<100; i++){
+ console.log("Maha Zainab");   
+}
+// let i=0;
+// while(i<100){
+//     console.log("Hello");
+//     i++;
+// }
+// let fruits=["Papaya","Apple","Mangoes", "Bnanas"];
+// for(let val of fruits){
+//     console.log(val);
+// }
+//let array = [1, 2, 3, 4]; for (let index in array) { console.log(array[index]); }
+// for(let i=0; i<5; i++){
+
+//     console.log("Hello");
+// }
+// const prod = { 0: 1, b: 2, c: 3 };
+// for (const property in object) {
+//   console.log(`${property}: ${object[property]}`);
+// }
+
+// let count =1;
+// while(count<5){
+//     console.log(count);
+//     count++;
+// }
+// // for of loops
+// // help to iterate through strings and arrays
+// let username= "Maha Zainab"
+// let size=0;
+// for(let val of username){
+//     console.log(val);
+//     size++;
+// }
+// console.log(size);
+
+// //Game to ask from user to enter a correct number
+// let num=4;
+// //val=prompt("Guss a number");
+// // while(val!=num){
+// //     val=prompt("Wrong number. Guess again");
+// // }
+// // console.log("Bravo");
