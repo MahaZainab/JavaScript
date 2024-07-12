9fa1fe2f55c83819d0323e1c7bc2fb2ff1a8d073
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-07-12 12:59:25 +0500

    Array declaration with different datatypes

--- Arrays.js
+++ Arrays.js
@@ -1,6 +1,14 @@
 /** Array is a collection of objects/items
  * if we want to store information of same type 
  * Linear method to store any information
+ * In JS we can add different datatypes in a single array but not prefered
  */
 let heros=["Maha", "Zainab", "IK", "QA","AI"];
+console.log(heros);
+console.log(typeof heros);
 let marks=[56,84,21,56,34,84];
+console.log(marks);
+console.log(typeof marks);
+let student1=["Asad",24,85];
+console.log(student1);
+console.log(typeof student1);
