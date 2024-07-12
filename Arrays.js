80ce18c7b99dbafa364764af63f5addf34680218
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-07-12 12:11:57 +0500

    Array declaration

---
+++ Arrays.js
@@ -0,0 +1,6 @@
+/** Array is a collection of objects/items
+ * if we want to store information of same type 
+ * Linear method to store any information
+ */
+let heros=["Maha", "Zainab", "IK", "QA","AI"];
+let marks=[56,84,21,56,34,84];
