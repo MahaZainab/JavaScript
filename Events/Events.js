63c9dc70c727c8c6112df6e2f85bfee32c1dc342
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-07-22 21:16:55 +0500

    HTML and JS file linking

---
+++ Events/Events.js
@@ -0,0 +1,11 @@
+// In this code we will understand the topic of Events in JavaScript
+/**
+ * he user selects, clicks, or hovers the cursor over a certain element.
+The user chooses a key on the keyboard.
+The user resizes or closes the browser window.
+A web page finishes loading.
+A form is submitted.
+A video is played, paused, or ends.
+An error occurs.
+ * 
+ *  */ 
