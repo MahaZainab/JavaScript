331146dc2bc9c354163139c9c7f138f43ca6e511
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-07-22 21:18:42 +0500

    Change Color

--- Events/Events.js
+++ Events/Events.js
@@ -9,3 +9,13 @@ A video is played, paused, or ends.
 An error occurs.
  * 
  *  */ 
+const btn = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+btn.addEventListener("click", () => {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
