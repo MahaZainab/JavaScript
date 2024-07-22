1e096b381d09058dfcce9b0e9853853b2ab8cc7e
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-07-22 21:20:29 +0500

    Removing Listener

--- Events/Events.js
+++ Events/Events.js
@@ -19,3 +19,5 @@ btn.addEventListener("click", () => {
   const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
   document.body.style.backgroundColor = rndCol;
 });
+
+// Removing Listener
